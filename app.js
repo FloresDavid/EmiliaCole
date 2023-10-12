8b2dea15becd5c778ea89374d70a6d48b99cff28
FloresDavid <Davidabelflores@gmail.com>
2023-10-12 09:56:37 -0300

    add structure initial

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path')
+const app = express();
+const PORT = 3030
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
+
+app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')));
+app.get('/contact',(req,res) => res.sendFile(path.join(__dirname,'views','contact.html')));
+app.get('/about',(req,res) => res.sendFile(path.join(__dirname,'views','about.html')));
+app.get('/music',(req,res) => res.sendFile(path.join(__dirname,'views','music.html')));
